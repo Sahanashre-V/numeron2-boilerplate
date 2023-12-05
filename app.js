25317cd56aed473a1a009a89c12ce3c49a8c8949
V.Sahanashre <sahanashre.v@kalvium.community>
2023-12-05 17:56:14 +0530

    Added

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 // Iteration 1: Making the play button in the index.html functional.
 // Description: When the play button is clicked the game.html page should be opened
+let playbutton= document.getElementById("play-button");
+playbutton.addEventListener("click",function(){
+    window.location.href="./game.html"
+})
--- score.js
+++ score.js
@@ -1,1 +1,7 @@
 // Iteration 8: Making scoreboard functional
+let scoreboard= document.getElementById("score-board");
+scoreboard.innerText=localStorage.getItem("score");
+let playAgainBtn=document.getElementById("play-again-button");
+playAgainBtn.addEventListener("click",function(){
+    window.location.href="./game.html"
+})
